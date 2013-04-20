99c2f9a764e70475b70f679d511cbfb4196d7a01
CentOS6.4 <jiangsh@localhost.localdomain>
2013-04-20 09:52:44 -0700

    Add Blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '54396321ad84eca5d3139cd56a944c44ded7a5ec011d15ad941b332d1776d6190ee3783925e12eb55366e0c74f4158016b70a086f59decb450615fa753f0d5f4'
